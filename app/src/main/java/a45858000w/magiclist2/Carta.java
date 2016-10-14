54535effc01650a9df47fb03f7d8c13e8ff9acc3
CristianJRamirez <45858000w@iespoblenou.org>
2016-10-14 17:03:39 +0200

    Crear Clase de la Carta, POJO

---
+++ app/src/main/java/a45858000w/magiclist2/Carta.java
@@ -0,0 +1,63 @@
+package a45858000w.magiclist2;
+
+/**
+ * Created by 45858000w on 14/10/16.
+ */
+
+public class Carta {
+
+    private String name;
+    private String manaCost;
+    private String type;
+    private String rarity;
+    private String text;
+    private String power;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManaCost() {
+        return manaCost;
+    }
+
+    public void setManaCost(String manaCost) {
+        this.manaCost = manaCost;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public void setPower(String power) {
+        this.power = power;
+    }
+}
