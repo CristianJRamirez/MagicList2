398fb54ef776de993d25d8bd0d3788e88342f621
CristianJRamirez <45858000w@iespoblenou.org>
2016-10-14 16:09:44 +0200

    Creamos el array que contendra la lista de cartas

--- app/src/main/java/a45858000w/magiclist2/MainActivityFragment.java
+++ app/src/main/java/a45858000w/magiclist2/MainActivityFragment.java
@@ -5,12 +5,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+
+    //region VARIABLES
+    private ArrayList<String> cartas;
+    private ArrayAdapter<String> adapter;
+    //endregion
+
+
 
     public MainActivityFragment() {
     }
@@ -21,6 +32,16 @@ public class MainActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         ListView listaCartas = (ListView) view.findViewById(R.id.listaCartas);
+
+        cartas = new ArrayList<>();
+        adapter = new ArrayAdapter<>(
+             getContext(),
+             R.layout.cartas_layout,
+             R.id.Carta,
+                cartas
+             );
+        listaCartas.setAdapter(adapter);
+
 
         return view;
     }
