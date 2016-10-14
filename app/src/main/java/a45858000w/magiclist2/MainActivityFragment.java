1675d7f456fdd03c27fe1e015bfddf710701c11a
CristianJRamirez <45858000w@iespoblenou.org>
2016-10-14 16:52:33 +0200

    Click en el boton Actualizar

--- app/src/main/java/a45858000w/magiclist2/MainActivityFragment.java
+++ app/src/main/java/a45858000w/magiclist2/MainActivityFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -73,4 +74,22 @@ public class MainActivityFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_cartas,menu);
     }
+
+
+    //region Click en el boton Actualizar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.actualizar) {
+            refresh();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+       private void refresh() {
+    }
+    //endregion
+    
 }
