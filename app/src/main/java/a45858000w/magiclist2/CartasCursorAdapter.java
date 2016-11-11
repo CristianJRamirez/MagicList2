e8e3439467db43153a17444e5a171838f2e88c2b
CristianJRamirez <45858000w@iespoblenou.org>
2016-11-11 16:24:14 +0100

    Creamos el CupboardCursorAdapter para mostrar los datos del ContentProvider

---
+++ app/src/main/java/a45858000w/magiclist2/CartasCursorAdapter.java
@@ -0,0 +1,45 @@
+package a45858000w.magiclist2;
+
+/**
+ * Created by 45858000w on 11/11/16.
+ */
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.graphics.Movie;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+
+
+
+import a45858000w.magiclist2.databinding.CartasLayoutBinding;
+
+public class CartasCursorAdapter extends CupboardCursorAdapter<Carta> {
+
+    public CartasCursorAdapter(Context context, Class<Carta> entityClass) {
+        super(context, entityClass);
+    }
+
+    @Override
+    public View newView(Context context, Carta model, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        CartasLayoutBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.cartas_layout, parent, false);
+
+        return binding.getRoot();
+    }
+
+
+    @Override
+    public void bindView(View view, Context context, Carta model) {
+        CartasLayoutBinding binding = DataBindingUtil.getBinding(view);
+
+        binding.Carta.setText(model.getName());
+        binding.power.setText(model.getPower());
+        binding.rarity.setText(model.getRarity());
+        Glide.with(context).load(model.getImageUrl()).into(binding.Image);
+    }
+}
