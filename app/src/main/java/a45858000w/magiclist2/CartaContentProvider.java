ac4038228a99dae612d59fc76843de30c3dafb8c
CristianJRamirez <45858000w@iespoblenou.org>
2016-11-11 15:45:40 +0100

    Creamos el Content Provider -> Clase/Interface para acceder a la BBDD

---
+++ app/src/main/java/a45858000w/magiclist2/CartaContentProvider.java
@@ -0,0 +1,25 @@
+package a45858000w.magiclist2;
+import android.graphics.Movie;
+
+import nl.littlerobots.cupboard.tools.provider.CupboardContentProvider;
+
+import static nl.qbusict.cupboard.CupboardFactory.cupboard;
+
+/**
+ * Created by 45858000w on 11/11/16.
+ */
+
+public class CartaContentProvider extends CupboardContentProvider {
+
+
+    // The content provider authority is used for building Uri's for the provider
+    public static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".provider";
+
+    static {
+        cupboard().register(Movie.class);
+    }
+
+    public CartaContentProvider() {
+        super(AUTHORITY, 1);
+    }
+}
